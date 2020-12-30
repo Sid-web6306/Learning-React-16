233a7d7ff71940f1fbff04f92bc6d1054151e2c8
unknown <siddhantjain15298@gmail.com>
2020-12-30 12:45:40 +0530

    Learning React-16

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>HELLO I AM LEARNING  REACT JS AND I WANT TO BECOME REACT JS DEVELOPER.</p>
+    </div>
+  );
+}
+
+export default App;
