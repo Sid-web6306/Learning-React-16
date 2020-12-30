1f0efa16bdbfcad508a664e8c4d6b21d8ced5b78
unknown <siddhantjain15298@gmail.com>
2020-12-30 17:58:34 +0530

    Learn About class component and jsx and understand React file Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>HELLO I AM LEARNING  REACT JS AND I WANT TO BECOME REACT JS DEVELOPER.</p>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    //Another Way of writing a JSX
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null, 'Hello, I m learning React-16.'));
+    return(
+      <div className="App">
+        <h1>hello , I m lerning  React-16</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
