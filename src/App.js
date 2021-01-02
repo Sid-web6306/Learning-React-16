cab582fef8092da411df0788c290adb9984f36cb
unknown <siddhantjain15298@gmail.com>
2021-01-02 19:02:14 +0530

    Using useState in functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from "./components/Person/Person";
 import './App.css';
+const App = props=> {
 
-export default class App extends Component {
-
-  state={
+  const [personsState,setpersonsState ] = useState({
     persons:[
-      {name:"Siddhant",age:23},{name:"Joe", age:40},{name:"Mili",age:12}
+         {name:"Siddhant",age:23},{name:"Joe", age:40},{name:"Mili",age:12}
     ],
+    otherState:"some Other Value"
+  })
+  // we can use as many useState as we want
+  // and when ee want to change state it overwrite previous state 
+  console.log(personsState);
+  const switchNameHandler = () => {
+    setpersonsState({
+      persons:[
+        {name:"Siddhant",age:23},{name:"Rose", age:24},{name:"Rachel",age:30}
+      ],
+      otherState: personsState.otherState
+    })
   }
 
-
-  switchNameHandler = () => console.log("Was clicked correctly");
-
-
-
-  render() {
     //Another Way of writing a JSX
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null, 'Hello, I m learning React-16.'));
     return(
@@ -23,12 +28,32 @@ export default class App extends Component {
         <h1>Hello , I m lerning  React-16</h1>
         <h3>This is so cool!!!</h3>
         {/* dont use paranthesis it will execute function immediatelty after rendering */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Football</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Football</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
 
       </div>
     );
-  }
 }
+
+export default App;
+
+
+// state={
+//     persons:[
+//       {name:"Siddhant",age:23},{name:"Joe", age:40},{name:"Mili",age:12}
+//     ],
+//   }
+
+
+//   switchNameHandler = () => {
+    
+//     // console.log("Was clicked correctly");
+//     //DONT DIRECTLY CHANGE THE STATE OBJECT
+//     this.setState({
+//       persons:[
+//         {name:"Siddhant",age:23},{name:"Rose", age:40},{name:"Monica",age:33}
+//       ]
+//     });
+//   }
